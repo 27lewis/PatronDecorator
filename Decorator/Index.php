ea619c53f24274d596e269f5eb8480c0d7d8eff0
lewis escorcia <106995911+27lewis@users.noreply.github.com>
2025-04-13 21:12:42 -0500

    Add files via upload

--- Decorator/Index.php
+++ Decorator/Index.php
@@ -1,47 +1,53 @@
 <!DOCTYPE html>
 <html lang="es">
+    <head>
+        <link rel="stylesheet" href="a.css">
+    </head>
     <body>
-        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        Tipo cono: <select name="tipo">
-                <option>Sencillo</option>
-                <option>Doble</option>
-            </select>
-            <br><br>
-            <input type="checkbox" name="SalsaChocolate">Salsa de Chocholate
-            <br><br>
-            <input type="checkbox" name="Fresas">Fresas
-            <br><br>
-            <input type="checkbox" name="GalletaOreo">Galleta Oreo
-            <br><br>
-            <input type="submit" value="Ordernar">
+        <h1>Heladeria Il Gelato</h1>
+        <div class="Container">
+            <img src="ppp323.webp"  alt="Helado">
+            <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <label>Tipo cono:</label>
+                <select name="tipo">
+                    <option>Sencillo</option>
+                    <option>Doble</option>
+                </select>
+                <br><br>
+                <input type="checkbox" name="SalsaChocolate"> Salsa de Chocolate
+                <br><br>
+                <input type="checkbox" name="Fresas"> Fresas
+                <br><br>
+                <input type="checkbox" name="GalletaOreo"> Galleta Oreo
+                <br><br>
+                <input type="submit" value="Ordenar">
+            </form>
+        </div>
 
-        </form>
-
+     
 
         <?php
-
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 require "Clases.php";
                 $tipo = $_POST['tipo'];
-                $helado = null;
-                if($tipo == "Sencillo"){
-                    $helado = new ConoSencillo();
-                }else{
-                    $helado = new ConoDoble();
-                }
+                $helado = $tipo == "Sencillo" ? new ConoSencillo() : new ConoDoble();
 
-                if(isset($_POST['SalsaChocolate'])){
+                if (isset($_POST['SalsaChocolate'])) {
                     $helado = new SalsaChocholate($helado);
                 }
-                if(isset($_POST['Fresas'])){
+                if (isset($_POST['Fresas'])) {
                     $helado = new Fresas($helado);
                 }
-                if(isset($_POST['GalletaOreo'])){
+                if (isset($_POST['GalletaOreo'])) {
                     $helado = new GalletaOreo($helado);
                 }
-                echo "<br>" . $helado->getDescripcion() . " vale " . $helado->getPrecio();
+
+                $mensaje = $helado->getDescripcion() . " vale " . $helado->getPrecio();
+
+                echo "<div class='mensaje-flotante'>$mensaje</div>";
             }
-        ?>
+         ?>
+
 
 
     </body>
